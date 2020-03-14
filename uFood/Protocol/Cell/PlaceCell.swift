fede4e4cb6f4b65a9faac61fe9f36d8dd75b914e
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-14 11:39:31 -0300

    creating protocol: PlaceCell, CellViewModel

---
+++ uFood/Protocol/Cell/PlaceCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceCell.swift
+//  uFood
+//
+//  Created by Elano on 11/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+protocol PlaceCell {
+
+    var nameLabel: UILabel { get }
+    var openLabel: UILabel { get }
+    var ratingLabel: UILabel { get }
+    
+}
---
+++ uFood/Protocol/ViewModel/CellViewModel.swift
@@ -0,0 +1,31 @@
+//
+//  CellViewModel.swift
+//  uFood
+//
+//  Created by Elano on 11/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+protocol CellViewModel {
+    var cellIdentifier: String { get }
+    
+    func configure(cell: UITableViewCell)
+}
+
+//MARK: -
+class Observable<T> {
+    var value: T {
+        didSet {
+            ThreadHelper.main {
+                self.valueChanged?(self.value)
+            }
+        }
+    }
+    var valueChanged: ((T) -> Void)?
+    
+    init(_ value: T) {
+        self.value = value
+    }
+}
