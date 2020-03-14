b6731daf100fc775c9488415ab7cc9af3775556c
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-14 18:52:03 -0300

    created: ImageCell protocol, ImageTableViewCell

---
+++ uFood/Protocol/Cell/ImageCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCell.swift
+//  uFood
+//
+//  Created by Elano on 14/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+protocol ImageCell {
+    var cellImageView: UIImageView! { get }
+}
---
+++ uFood/View/Cell/ImageTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageTableViewCell.swift
+//  uFood
+//
+//  Created by Elano on 14/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class ImageTableViewCell: UITableViewCell, ImageCell {
+
+    @IBOutlet var cellImageView: UIImageView!
+    
+}
