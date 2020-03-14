31018854185e38e4c415e6190f93ff3f9d957a13
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-14 10:18:39 -0300

    making project compatible with iOS 11; created: PlaceListViewController;

---
+++ uFood/Controller/PlaceListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceListViewController.swift
+//  uFood
+//
+//  Created by Elano on 14/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+    
+
+    
+
+}
--- uFood/Delegate/AppDelegate.swift
+++ uFood/Delegate/AppDelegate.swift
@@ -11,21 +11,41 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        if #available(iOS 13, *) {
+          // nothing, SceneDelegate setup
+        } else {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = AppDelegate.initialViewController()
+            self.window?.makeKeyAndVisible()
+        }
+        
         return true
     }
 
+    static func initialViewController() -> UIViewController {
+        
+        let mainViewController = PlaceListViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+
+        mainViewController.willMove(toParent: navigationController)
+        
+        return navigationController
+    }
+    
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
