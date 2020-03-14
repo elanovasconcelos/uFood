fc654a79c5bb1ce272e60962c7a236014bc6db1c
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-14 18:20:55 -0300

    created: PlaceTableViewCell

---
+++ uFood/View/Cell/PlaceTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceTableViewCell.swift
+//  uFood
+//
+//  Created by Elano on 11/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceTableViewCell: UITableViewCell, PlaceCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var openLabel: UILabel!
+    @IBOutlet var ratingLabel: UILabel!
+    
+}
