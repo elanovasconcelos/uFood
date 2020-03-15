4be8834ed0a5674159821411753acc51bb94ea3a
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-15 11:09:27 -0300

    created: PlaceDetailViewModel, PlaceDetailViewController

---
+++ uFood/Controller/PlaceDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PlaceDetailViewController.swift
+//  uFood
+//
+//  Created by Elano on 14/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceDetailViewController: BaseViewController {
+
+    private let placeDetailViewModel: PlaceDetailViewModel
+    
+    //MARK: -
+    init(placeDetailViewModel: PlaceDetailViewModel) {
+        self.placeDetailViewModel = placeDetailViewModel
+        
+        super.init(cellIdentifiers: placeDetailViewModel.cellIdentifiers)
+        
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setup() {
+        setupBinding()
+    }
+
+    private func setupBinding() {
+        placeDetailViewModel.cellModels.valueChanged = { [weak self] newModels in
+            print("valueChanged")
+            self?.items = newModels
+        }
+    }
+}
---
+++ uFood/ViewModel/Controller/PlaceDetailViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  PlaceDetailViewModel.swift
+//  uFood
+//
+//  Created by Elano on 14/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+
+final class PlaceDetailViewModel: NSObject {
+
+    private let server: RequestImage
+    
+    let imageCellViewModel = ImageCellViewModel()
+    let cellModels = Observable<[CellViewModel]>([])
+    let cellIdentifiers = [PlaceCellViewModel.cellIdentifier,
+                           ImageCellViewModel.cellIdentifier]
+    
+    
+    init(place: Place, server: RequestImage = PlaceModel()) {
+        self.server = server
+        super.init()
+        
+        setupModel(place: place)
+        requestImage(photos: place.photos)
+    }
+    
+    private func setupModel(place: Place) {
+        cellModels.value = [imageCellViewModel,
+                            PlaceCellViewModel(place: place)]
+    }
+    
+    private func requestImage(photos: [Photo]) {
+        guard let reference = photos.first?.photoReference else {
+            imageCellViewModel.isLoading.value = false
+            imageCellViewModel.image.value = nil
+            return
+        }
+        
+        print("width: \(String(describing: photos.first?.width)), height: \(String(describing: photos.first?.height))")
+        imageCellViewModel.isLoading.value = true
+        
+        server.image(photoReference: reference, maxWidth: 750) { [weak self] (result) in
+            self?.imageCellViewModel.isLoading.value = false
+            switch result {
+            case .failure(let error):
+                print("requestImage|error: \(error)")
+                self?.imageCellViewModel.image.value = nil
+            case .success(let image):
+                self?.imageCellViewModel.image.value = image
+            }
+        }
+    }
+}
