67126a142e5a6156288c50a738e33af954fef5e6
Elano Vasconcelos <elanovasconcelos@gmail.com>
2020-03-18 21:58:16 -0300

    created: LocationModel

---
+++ uFood/Model/LocationModel.swift
@@ -0,0 +1,64 @@
+//
+//  LocationModel.swift
+//  uFood
+//
+//  Created by Elano on 18/03/20.
+//  Copyright © 2020 Elano. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+protocol LocationModelDelegate: class {
+    func locationModel(_ locationModel: LocationModel, didChange location: Location)
+}
+
+final class LocationModel: NSObject {
+    
+    private let locationManager: CLLocationManager
+    
+    weak var delegate: LocationModelDelegate?
+    
+    override init() {
+        locationManager = CLLocationManager()
+        
+        super.init()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+    }
+    
+    func requestWhenInUseAuthorization() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func locationServicesEnabled() -> Bool {
+        return CLLocationManager.locationServicesEnabled()
+    }
+
+    /// It starts trying to get a location. The location will return by the delegate just one time if possible.
+    func updateLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopUpdateLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+//MARK: - CLLocationManagerDelegate
+extension LocationModel: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        stopUpdateLocation()
+        guard let coordinate = manager.location?.coordinate else { return }
+        
+        let location = Location(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        delegate?.locationModel(self, didChange: location)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("[LocationModel] error: \(error.localizedDescription)")
+        //TODO: show error to user
+    }
+}
